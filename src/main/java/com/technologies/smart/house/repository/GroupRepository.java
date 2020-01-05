3dca0fae77520d4b8a742b5315bf7f17747e1a26
anton-isupov <tony.isupov2010@yandex.ru>
2020-01-05 14:35:13 +0300

    Change.

--- src/main/java/com/technologies/smart/house/repository/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.technologies.smart.house.repository;
-
-import com.technologies.smart.house.model.Group;
-import org.springframework.data.repository.CrudRepository;
-
-public interface GroupRepository extends CrudRepository<Group, Long> {
-}
--- src/main/java/com/technologies/smart/house/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.technologies.smart.house.repository;
-
-import com.technologies.smart.house.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
